e04460426e09aad71bff90ca619a913f9f841970
Ali Shah <ali1195shah@gmail.com>
2019-09-04 19:09:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,6 +10,7 @@ def run_guessing_game
   if input.length < 2
     if input.to_i == random_num
       puts "You guessed the correct number!"
+      
     elsif input.to_i != random_num
       puts "Sorry! The computer guessed #{random_num}."
     end
